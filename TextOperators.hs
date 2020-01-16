bd32e3896119d725e11716514b6dde67f3d64d02
RawIron <manianology@gmail.com>
2020-01-16 14:01:54 -0800

    Use hunit and quickcheck for tests

--- TextOperators.hs
+++ TextOperators.hs
@@ -3,36 +3,26 @@ module Main where
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Text as T
-import Data.List as L
-import Prelude as P
 -- convert String to Text
 -- convert Text to String
 import qualified Data.Text.Conversions as TC
 
+import Test.QuickCheck
+import Test.QuickCheck.Instances
+import Test.HUnit
+
 isPalindrom :: Text -> Bool
 isPalindrom word = word == T.reverse word
 
-chainBracket :: [Int] -> Int
-chainBracket counters = ((+1) . L.head) counters
 
-chainDot :: [Int] -> Int
-chainDot counters = (+1) $ L.head counters
+prop_reverseInvariant word = isPalindrom word == (isPalindrom $ T.reverse word)
 
-
-testChain:: IO ()
-testChain = do
-  let c = chainBracket [2,4]
-  let d = chainDot [4,4]
-  print (c,d)
-
-testPalindrome :: IO ()
-testPalindrome = do
-  let a = (isPalindrom . TC.convertText) "(hello)"
-  print (a)
-  let b = isPalindrom $ TC.convertText "$ollo$"
-  print (b)
+test_palindrome_fail = TestCase (assertBool "hello should fail" (not . isPalindrom $ TC.convertText "(hello)"))
+test_palindrome_ok = TestCase (assertBool "ollo should pass" (isPalindrom $ TC.convertText "$ollo$"))
+tests = TestList [test_palindrome_fail, test_palindrome_ok]
 
 main :: IO ()
 main = do
-  testChain
-  testPalindrome
+  quickCheck prop_reverseInvariant
+  runTestTT tests
+  print ""
