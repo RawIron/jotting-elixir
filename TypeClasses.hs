6ccf199f5a71b047d65a74d11ef682127fc344dd
RawIron <manianology@gmail.com>
2021-04-10 13:43:47 -0700

    Haskell TypeClasses - add GetMany

--- TypeClasses.hs
+++ TypeClasses.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
 
 
@@ -10,9 +12,29 @@ instance (Num a) => Semigroup (Card a) where
   (<>) = power
 
 
+class (Read a) => GetMany a where
+  getMany :: IO [a]
+
+instance GetMany Int where
+  getMany = do
+    _ <- read <$> getLine :: IO Int
+    (map read . words) <$> getLine
+
+instance GetMany String where
+  getMany = do
+    _ <- read <$> getLine :: IO Int
+    words <$> getLine
+
+
 main :: IO ()
 main = do
   let king = Card 2 3
       queen = Card 1 3
       royal = king <> queen
   putStrLn $ show royal
+
+  names <- getMany :: IO [String]
+  mapM_ putStrLn names
+
+  numbers <- getMany :: IO [Int]
+  mapM_ (putStrLn . show) numbers
